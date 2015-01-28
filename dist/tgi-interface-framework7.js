4cf0ba83b9b3556504fc6004ac8a1dd5bdcb59b0
Sean Smith <sean@tgicloud.com>
2015-01-28 15:20:02 -0500

    crack is wack

--- dist/tgi-interface-framework7.js
+++ dist/tgi-interface-framework7.js
@@ -10,7 +10,7 @@ var root = this;
 var TGI = {
   CORE: function () {
     return {
-      version: '0.0.33',
+      version: '0.0.34',
       Application: Application,
       Attribute: Attribute,
       Command: Command,
@@ -605,6 +605,10 @@ Command.prototype.abort = function () {
 };
 Command.prototype.complete = function () {
   this.status = 1;
+  this._emitEvent('Completed');
+};
+Command.prototype.restart = function () {
+  // TODO add to tests
   this._emitEvent('Completed');
 };
 /**
@@ -2462,6 +2466,152 @@ Framework7Interface.prototype.start = function (application, presentation, callB
   this.presentation = presentation;
   this.startCallback = callBack;
   if (!this.vendor) throw new Error('Error initializing Framework7');
+  try {
+    if (!Framework7Interface._f7) {
+      Framework7Interface._f7 = new this.vendor();
+    }
+  } catch (e) {
+    throw new Error('Error initializing Framework7: ' + e);
+  }
+};
+Framework7Interface.prototype.info = function (text) {
+  if (!text || typeof text !== 'string') throw new Error('text required');
+  Framework7Interface._f7.addNotification({title: this.application.get('brand'), message: text, hold:3000});
+};
+Framework7Interface.prototype.ok = function (prompt, callBack) {
+  if (!prompt || typeof prompt !== 'string') throw new Error('prompt required');
+  if (typeof callBack != 'function') throw new Error('callBack required');
+  if (this.okPending) {
+    delete this.okPending;
+    callBack();
+  } else {
+    Framework7Interface._f7.alert(prompt.replace(/\n/g,'<br>'), this.application.get('brand'), function () {
+      callBack();
+    });
+    //this.okCallBack = callBack;
+  }
+};
+Framework7Interface.prototype.yesno = function (prompt, callBack) {
+  if (!prompt || typeof prompt !== 'string') throw new Error('prompt required');
+  if (typeof callBack != 'function') throw new Error('callBack required');
+  if (this.yesnoPending) {
+    delete this.yesnoPending;
+    callBack(this.yesnoResponse);
+  } else {
+    Framework7Interface._f7.modal({
+      text:prompt.replace(/\n/g,'<br>'),
+      title: this.application.get('brand'),
+      verticalButtons: true,
+      buttons: [{
+        text: 'Yes',
+        onClick: function () {
+          callBack(true);
+        }
+      },{
+        text: 'No',
+        onClick: function () {
+          callBack(false);
+        }
+      }]
+    });
+    //this.yesnoCallBack = callBack;
+  }
+};
+Framework7Interface.prototype.ask = function (prompt, attribute, callBack) {
+  if (!prompt || typeof prompt !== 'string') throw new Error('prompt required');
+  if (false === (attribute instanceof Attribute)) throw new Error('instance of Attribute a required parameter');
+  if (typeof callBack != 'function') throw new Error('callBack required');
+  if (this.askPending) {
+    delete this.askPending;
+    callBack(this.askResponse);
+  } else {
+    Framework7Interface._f7.prompt(prompt.replace(/\n/g,'<br>'), this.application.get('brand'),
+      function (answer) {
+        callBack(answer);
+      },
+      function () {
+        callBack();
+      }
+    );
+    //this.askCallBack = callBack;
+  }
+};
+Framework7Interface.prototype.choose = function (prompt, choices, callBack) {
+  if (!prompt || typeof prompt !== 'string') throw new Error('prompt required');
+  if (false === (choices instanceof Array)) throw new Error('choices array required');
+  if (!choices.length) throw new Error('choices array empty');
+  if (typeof callBack != 'function') throw new Error('callBack required');
+  if (this.choosePending) {
+    delete this.choosePending;
+    callBack(Interface.firstMatch(this.chooseResponse, choices));
+  } else {
+
+    var groups = [];
+    groups.push([{text: prompt.replace(/\n/g,'<br>'), label: true}]);
+    if (choices.length > 0) groups.push([{text: choices[0], onClick: cb0}]);
+    if (choices.length > 1) groups.push([{text: choices[1], onClick: cb1}]);
+    if (choices.length > 2) groups.push([{text: choices[2], onClick: cb2}]);
+    if (choices.length > 3) groups.push([{text: choices[3], onClick: cb3}]);
+    if (choices.length > 4) groups.push([{text: choices[4], onClick: cb4}]);
+    if (choices.length > 5) groups.push([{text: choices[5], onClick: cb5}]);
+    if (choices.length > 6) groups.push([{text: choices[6], onClick: cb6}]);
+    if (choices.length > 7) groups.push([{text: choices[7], onClick: cb7}]);
+    if (choices.length > 8) groups.push([{text: choices[8], onClick: cb8}]);
+    if (choices.length > 9) groups.push([{text: choices[9], onClick: cb9}]);
+    if (choices.length > 10) throw new Error('max choices reached in choose');
+    groups.push([{text: 'Cancel', color: 'red', onClick: cbCancel}]);
+
+    Framework7Interface._f7.actions(groups);
+
+    //this.chooseCallBack = callBack;
+    //this.chooseChoices = choices;
+  }
+  /**
+   * Since framework does not return any info in callback
+   */
+  function cbCancel() {
+    callBack();
+  }
+
+  function cb0() {
+    callBack(choices[0]);
+  }
+
+  function cb1() {
+    callBack(choices[1]);
+  }
+
+  function cb2() {
+    callBack(choices[2]);
+  }
+
+  function cb3() {
+    callBack(choices[3]);
+  }
+
+  function cb4() {
+    callBack(choices[4]);
+  }
+
+  function cb5() {
+    callBack(choices[5]);
+  }
+
+  function cb6() {
+    callBack(choices[6]);
+  }
+
+  function cb7() {
+    callBack(choices[7]);
+  }
+
+  function cb8() {
+    callBack(choices[8]);
+  }
+
+  function cb9() {
+    callBack(choices[9]);
+  }
 };
 /**---------------------------------------------------------------------------------------------------------------------
  * tgi-interface-framework7/lib/_packaging/lib-footer
